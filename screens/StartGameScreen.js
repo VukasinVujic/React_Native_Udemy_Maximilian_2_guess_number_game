af379fe7f24dfc0ac1a1ce5ed36b3b229829bf1c
VukasinVujic <vukasinvujic021@gmail.com>
2020-06-30 10:17:19 +0200

    litle bit of shadow

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 const StartGameScreen = () => {
   return (
@@ -31,6 +38,14 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "white",
+    elevation: 5,
+    padding: 20,
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
