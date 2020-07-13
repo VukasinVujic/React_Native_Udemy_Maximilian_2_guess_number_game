4fb6931dbc758f11f757a5f1b91f2163592d4528
VukasinVujic <vukasinvujic021@gmail.com>
2020-07-13 19:02:39 +0200

    Add picture localy to GameOverScreen

--- App.js
+++ App.js
@@ -44,6 +44,14 @@ export default function App() {
 
   let content = <StartGameScreen onStarGame={startGameHandler} />;
 
+  content = (
+    <GameOverScreen
+      roundsNumber={1}
+      userNumber={1}
+      onRestart={configureNewGameHandler}
+    />
+  );
+
   if (userNumber && guessRounds <= 0) {
     content = (
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
