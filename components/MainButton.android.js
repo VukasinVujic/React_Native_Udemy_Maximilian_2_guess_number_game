dc67aa6f427e043c054496fefb35c0689d51f56f
VukasinVujic <vukasinvujic021@gmail.com>
2020-08-26 11:12:50 +0200

    Add two MainButton files one for Andriod other IOS

--- components/MainButton.android.js
+++ components/MainButton.android.js
@@ -15,7 +15,7 @@ import Colors from "../constants/colors";
 const MainButton = (props) => {
   let ButtonComponent = TouchableOpacity;
   // riple effect only works on Versions bigger then 21
-  if (Platform.OS === "android" && Platform.Version >= 21) {
+  if (Platform.Version >= 21) {
     ButtonComponent = TouchableNativeFeedback;
   }
   return (
